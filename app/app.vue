28495e994227051b2fec42596458cf6372623d43
kobino <felipe.arg@gmail.com>
2024-10-28 16:12:46 +0100

    chore(app): Added layout and routing to the entry point component.

--- app/app.vue
+++ app/app.vue
@@ -1,27 +1,5 @@
 <template>
-  <main>
-    <div class="uno">UNO</div>
-    <div class="dos">DOS</div>
-  </main>
+  <NuxtLayout name="default">
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-<script setup lang="ts">
-const baseRecipe: IngredientList = {
-  test: 500,
-};
-const emptyRecipe: IngredientList = {};
-</script>
-<style lang="postcss">
-main {
-  border: 2px solid var(--color-secondary);
-  > .uno {
-    border: 3px solid var(--color-primary);
-  }
-}
-</style>
-<style lang="scss">
-main {
-  .dos {
-    border: 3px solid var(--color-accent);
-  }
-}
-</style>
