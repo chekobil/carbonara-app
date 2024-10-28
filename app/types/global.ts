8608966a72d97f08debe648c26b98be69ee47afb
kobino <felipe.arg@gmail.com>
2024-10-28 16:12:56 +0100

    types()

--- app/types/global.ts
+++ app/types/global.ts
@@ -2,5 +2,11 @@ declare global {
   type IngredientList = {
     test?: Number;
   };
+  interface Store {
+    id: Number;
+    name: string;
+    location: string;
+  }
+  type StoreList = Store[];
 }
 export {};
