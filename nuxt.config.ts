7dcccf1629989fd8ec606a2f425c8fcdac04121e
kobino <felipe.arg@gmail.com>
2024-10-28 16:47:41 +0100

    chore(tests): Baasic testing with Vitest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4,
   },
+  modules: ["@nuxt/test-utils/module"],
   postcss: {
     plugins: {
       "postcss-preset-env": postcssPresetEnv(),
