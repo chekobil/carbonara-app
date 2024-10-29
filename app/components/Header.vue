bead350cb6c9514dc6b1e4ef610d898d778e2012
kobino <felipe.arg@gmail.com>
2024-10-29 12:17:24 +0100

    refactor(): Header and Footer as independent components

---
+++ app/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <div class="navbar-center lg:flex">
+      <ul class="menu menu-horizontal px-1 w-full">
+        <li><a href="/">Home</a></li>
+        <li><a href="/stores">Stores</a></li>
+        <li
+          class="flex-1 flex-row justify-end"
+          @click="emit('change-theme', null)"
+        >
+          <a>Theme</a>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits<{
+  "change-theme": [null];
+}>();
+</script>
