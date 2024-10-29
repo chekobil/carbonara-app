506cc481188206b7443c3e659ac193b2994debb7
kobino <felipe.arg@gmail.com>
2024-10-29 14:35:08 +0100

    refactor(Header): Navigation must be managed by the router

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -2,8 +2,8 @@
   <header>
     <div class="navbar-center lg:flex">
       <ul class="menu menu-horizontal px-1 w-full">
-        <li><a href="/">Home</a></li>
-        <li><a href="/stores">Stores</a></li>
+        <li><a href="#" @click.prevent="handleGoTo('/')">Home</a></li>
+        <li><a href="#" @click.prevent="handleGoTo('/stores')">Stores</a></li>
         <li
           class="flex-1 flex-row justify-end"
           @click="emit('change-theme', null)"
@@ -16,7 +16,12 @@
 </template>
 
 <script setup lang="ts">
+const router = useRouter();
 const emit = defineEmits<{
   "change-theme": [null];
 }>();
+
+const handleGoTo = (newPath: string) => {
+  router.push({ path: String(newPath) });
+};
 </script>
