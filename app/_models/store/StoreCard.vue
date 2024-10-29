7d80f1fde6a321cf5cad1df0b0ea8150094cf0a0
kobino <felipe.arg@gmail.com>
2024-10-29 19:16:27 +0100

    refactor(store): StoreCard component to show every store info

---
+++ app/_models/store/StoreCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="store-item card card-compact w-full shadow-xl">
+    <figure>
+      <img
+        :src="`https://picsum.photos/id/${randomNumber(1, 100)}/200/120`"
+        alt="Shoes"
+      />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">
+        <Icon name="uil:github" size="1.4rem" />{{ storeData.name }}
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { randomNumber } from "~/utils/numbers";
+const props = defineProps<{
+  storeData: Store;
+}>();
+</script>
+
+<style lang="scss">
+.store-item {
+  content-visibility: auto;
+  figure {
+    img {
+      width: 100%;
+      aspect-ratio: 16/9;
+    }
+  }
+}
+</style>
