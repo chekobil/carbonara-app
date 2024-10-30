cd56a6fe0af37986128f10bb46d0c6b4af778794
kobino <felipe.arg@gmail.com>
2024-10-30 12:52:12 +0100

    refactor(StoreSearch): Component is an input that emits on change

--- app/_models/store/StoreSearch.vue
+++ app/_models/store/StoreSearch.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>SEARCH</div>
+  <div class="store-search-container">
+    <input
+      type="text"
+      v-model="inputValue"
+      placeholder="Search store"
+      class="input input-bordered w-full max-w-xs"
+      @change="handleChangeInput"
+    />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const inputValue: Ref<string> = ref("");
+const emit = defineEmits<{
+  change: [value: string];
+}>();
+const handleChangeInput = () => {
+  emit("change", unref(inputValue));
+};
+</script>
 
-<style scoped></style>
+<style lang="scss">
+.store-search-container {
+  padding: 0.4rem 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
