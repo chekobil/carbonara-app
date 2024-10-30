87d079479664ab73a99850db2356b713e3c331d5
kobino <felipe.arg@gmail.com>
2024-10-30 12:53:14 +0100

    refactor(Footer)

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,5 +1,15 @@
 <template>
-  <footer>footer</footer>
+  <footer>
+    <div class="footer-credits">Company Inc.</div>
+  </footer>
 </template>
 
 <script setup lang="ts"></script>
+<style lang="scss">
+.footer-credits {
+  width: 100%;
+  font-size: var(--font-size-sm);
+  text-align: right;
+  color: oklch(var(--p));
+}
+</style>
