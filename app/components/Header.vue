d8f3fbfa4e49e84afb9094babd67a04b1be5bd77
kobino <felipe.arg@gmail.com>
2024-10-30 12:53:53 +0100

    refactor(Header): Added swap functionality to the change-theme button, and added icons

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -2,13 +2,34 @@
   <header>
     <div class="navbar-center lg:flex">
       <ul class="menu menu-horizontal px-1 w-full">
-        <li><a href="#" @click.prevent="handleGoTo('/')">Home</a></li>
-        <li><a href="#" @click.prevent="handleGoTo('/stores')">Stores</a></li>
-        <li
-          class="flex-1 flex-row justify-end"
-          @click="emit('change-theme', null)"
-        >
-          <a>Theme</a>
+        <li>
+          <a href="#" @click.prevent="handleGoTo('/')">
+            <Icon name="tabler:home" size="1.2rem" />
+          </a>
+        </li>
+        <li>
+          <a href="#" @click.prevent="handleGoTo('/stores')"
+            ><Icon name="tabler:building-store" size="1.2rem" />Our stores</a
+          >
+        </li>
+        <li class="flex-1 flex-row justify-end">
+          <label class="swap swap-rotate">
+            <input type="checkbox" @click="emit('change-theme', null)" />
+            <span class="swap-off">
+              <Icon
+                name="material-symbols:wb-sunny-outline-rounded"
+                mode="svg"
+                size="1.4rem"
+              />
+            </span>
+            <span class="swap-on">
+              <Icon
+                name="material-symbols:dark-mode-outline-rounded"
+                mode="svg"
+                size="1.4rem"
+              />
+            </span>
+          </label>
         </li>
       </ul>
     </div>
