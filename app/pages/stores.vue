a028879f2e1a5d82b456c1ce283d86c009ac2e6c
kobino <felipe.arg@gmail.com>
2024-10-30 12:56:10 +0100

    refactor(stores): Added search functionality and review styles

--- app/pages/stores.vue
+++ app/pages/stores.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="stores-container" ref="storeListElm">
+  <section class="stores-container" ref="storeListElm">
     <QuoteCard />
-    <h1>Stores</h1>
     <div v-if="!storeList.length">Loading store list ...</div>
-    <div v-else>
-      <StoreSearch />
-      <StoreList :store-list="storeList" />
+    <div v-else class="store-list-container">
+      <div class="title-group">
+        <h1>Stores</h1>
+        <StoreSearch @change="handleSearchStore" />
+      </div>
+      <StoreList :store-list="filteredStoreList || storeList" />
     </div>
-  </div>
+  </section>
 </template>
 
 <script setup lang="ts">
@@ -15,8 +17,10 @@ import { useInfiniteScroll, useDebounceFn } from "@vueuse/core";
 const { fetchStores } = useFetchStores();
 
 const storeList: Ref<StoreList> = ref([]);
+const filteredStoreList: Ref<StoreList | null> = ref(null);
 const currentPage: Ref<number> = ref(1);
 const storeListElm: Ref<HTMLElement | null> = ref(null);
+const searchQuery: Ref<string> = ref("");
 
 const { reset } = useInfiniteScroll(
   storeListElm,
@@ -41,6 +45,21 @@ const getMoreStores = async () => {
   currentPage.value++;
   const res = await fetchStores(currentPage.value);
   storeList.value = [...storeList.value, ...res];
+  if (searchQuery.value) handleSearchStore(searchQuery.value);
+};
+
+const handleSearchStore = (query: string) => {
+  searchQuery.value = query;
+  if (!query) {
+    filteredStoreList.value = null;
+    return;
+  }
+  filteredStoreList.value = unref(storeList).filter((store) =>
+    store.name.includes(query)
+  );
+  if (filteredStoreList.value.length < 4) {
+    getMoreStores();
+  }
 };
 </script>
 
@@ -50,15 +69,35 @@ const getMoreStores = async () => {
   flex: 0;
   display: flex;
   flex-direction: column;
-  gap: 1rem;
+  gap: 1.4rem;
   justify-content: flex-start;
   align-items: center;
   > div {
     width: 100%;
   }
+  .store-list-container {
+    display: flex;
+    flex-direction: column;
+    gap: 1.4rem;
+    justify-content: flex-start;
+    align-items: center;
+  }
   h1 {
     font-weight: 600;
     font-size: 2.4rem;
+  }
+  .title-group {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 0;
+    h1 {
+      flex: 1;
+      text-align: center;
+    }
+    .store-search-container {
+      flex: 1;
+    }
   }
 }
 </style>
