ee31d6718e8bd36f10424755747330536ad24303
kobino <felipe.arg@gmail.com>
2024-11-03 18:07:10 +0100

    refactror(useFetchStores): Added functionality to filter data when fetching.

--- app/_models/store/useFetchStores.ts
+++ app/_models/store/useFetchStores.ts
@@ -1,17 +1,27 @@
 let dataLength: number = 0;
 export function useFetchStores() {
-  const fetchStores = async (page: number) => {
-    console.info("fetchStores, page", page);
+  const fetchStores = async (page: number, query: string = "") => {
+    console.info("fetchStores, page", page, "with query", query);
     const itemsPerPage = 30;
     const sliceStart = (page - 1) * itemsPerPage;
     const sliceEnd = page * itemsPerPage;
-    let data: {
+    let dataRaw: {
       default: any[];
     } | null = await import("~/_models/store/stores.json");
-    dataLength = data.default.length;
-    const dataPaginated = data.default.slice(sliceStart, sliceEnd);
-    data = null;
-    return dataPaginated;
+    let data: any[] = dataRaw.default;
+    if (Boolean(query)) {
+      data = dataRaw.default.filter((item) =>
+        item.name.toLowerCase().includes(query.toLowerCase())
+      );
+    }
+    dataLength = data.length;
+    const maxPage: number = Math.ceil(dataLength / itemsPerPage);
+    const dataPaginated = data.slice(sliceStart, sliceEnd);
+    data = [];
+    if (page > maxPage) {
+      return { data: [], total: dataLength };
+    }
+    return { data: dataPaginated, total: dataLength };
   };
   return {
     fetchStores,
